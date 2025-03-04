b90fbf97e6da9081a3834cdb1c5181b6149e11e3
SnowyOchole <annabelleochotta@gmail.com>
2025-03-04 01:46:34 -0500

    added sort func

--- url_shortener_app/models/url.py
+++ url_shortener_app/models/url.py
@@ -16,6 +16,7 @@ class URL:
     
     @staticmethod
     def get_user_favs(user_id):
+        #can't add fav sort here or else favorite items may be out of order when history is viewed
         return get_mongo().db.urls.find({"user_id": user_id, "favorite": True})
     
     @staticmethod
